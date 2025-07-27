b7631e29aaba8ea0f27a4bb25b7ba49a6d2c8eec
dualspectre <education.dual@gmail.com>
2025-07-27 21:53:22 +0300

    add function normalize_phone in HW_3\main.py

--- PythonCore/HW_3/main.py
+++ PythonCore/HW_3/main.py
@@ -59,5 +59,33 @@ def get_numbers_ticket(min: int, max: int, quantity: int) -> list:
     return list(result_tickets)
     
    
-lottery_numbers = get_numbers_ticket(5,'10','50')
-print(lottery_numbers)
+# lottery_numbers = get_numbers_ticket(5,'10','50')
+# print(lottery_numbers)
+
+
+import re
+
+def normalize_phone(phone_number: str) ->str:
+    cleaned_number = re.sub(r'[^0-9+]','',phone_number)
+    if not cleaned_number.startswith('+'):
+        if cleaned_number.startswith('380'):
+            cleaned_number = '+' + cleaned_number
+        else:
+            cleaned_number = '+38' + cleaned_number
+    return cleaned_number
+    
+
+# raw_numbers = [
+#     "067\\t123 4567",
+#     "(095) 234-5678\\n",
+#     "+380 44 123 4567",
+#     "380501234567",
+#     "    +38(050)123-32-34",
+#     "     0503451234",
+#     "(050)8889900",
+#     "38050-111-22-22",
+#     "38050 111 22 11   ",
+# ]
+
+# norm_numbers = [normalize_phone(num) for num in raw_numbers]
+# print(norm_numbers)
